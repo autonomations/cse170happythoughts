abd3c611eb89c7d27cef0d26038080f3ff7fc2f1
Gabriela Ponce <gaponce@eng.ucsd.edu>
2014-03-14 05:48:14 +0000

    adding files that shouldn't affect anything..

--- routes/userData.js
+++
@@ -1,5 +0,0 @@
-var userData = require('../userData.json');
-
-exports.view = function(req, res){
-   res.json(userData);
-}
---
+++ routes/userData2.js
@@ -0,0 +1,9 @@
+/*aYUda:
+var userData = require('../userData.json');
+
+exports.userStress = function(req, res){
+	console.log("I AM INSIDE OF INDEX.JS!!!!!!");
+	var theStress = userData[0];
+	console.log(theStress);
+	res.json(theStress);
+}*/
